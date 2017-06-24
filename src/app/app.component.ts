4935febe2208c1541c1da2fe4e0f5ac8d856a604
Prashant S Khodade <prashantskhodade@Prashants-MacBook-Air.local>
2017-06-24 16:09:17 +0530

    First drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+
+import firebase from 'firebase';
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -11,12 +14,23 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyBbK3cIjwQh7DmUwXKp4_ONyF1cFx1s0sA",
+      authDomain: "hackathaon-pune.firebaseapp.com",
+      databaseURL: "https://hackathaon-pune.firebaseio.com",
+      projectId: "hackathaon-pune",
+      storageBucket: "hackathaon-pune.appspot.com",
+      messagingSenderId: "396284685494"
+    });
+
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  
+  message:string; 
+  encMessage:string; 
+  decMessage:string; 
+  
   constructor(public navCtrl: NavController) {
+    firebase.database().ref('/messages/encrypted').on('value', (snapshot) => {
+      this.encMessage = snapshot.val();
+    })
 
+    firebase.database().ref('/messages/decrypted').on('value', (snapshot) => {
+      this.decMessage = snapshot.val();
+    })
+  }
+
+  onEncrypt() {
+    console.log(this.message);
+    if (this.message != null) {
+      // Get the Database service for the default app
+      /* var defaultDatabase = */ firebase.database().ref('/messages').set({
+        original: this.message
+      });
+    }
   }
 
 }
